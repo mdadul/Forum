f08d378a74e3573884a529bff6f771591697e339
Emdadul <emdadulislam580@gmail.com>
2024-06-01 22:28:13 +0600

    chore: Add dialog component for displaying comments in post card

--- components/brand/post/index.tsx
+++ components/brand/post/index.tsx
@@ -1,5 +1,14 @@
 "use client";
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { PostWithComments } from "@/types";
 import { MessageCircle, User2 } from "lucide-react";
 import React from "react";
@@ -42,7 +51,35 @@ export default function PostCard({ post }: { post: PostWithComments }) {
 
       <div className="flex gap-1 items-center justify-end text-primary hover:underline cursor-pointer">
         <MessageCircle className="size-6" />
-        <p>{post.comments.length}</p>
+        <Dialog>
+          <DialogTrigger>{post.comments.length}</DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>
+                Comments for post: {post.title.slice(0, 30)}...
+              </DialogTitle>
+              <DialogDescription>
+                {post.comments.length} comments
+              </DialogDescription>
+            </DialogHeader>
+            <ScrollArea className="h-[calc(90vh-8rem)] pb-6">
+              <div className="flex flex-col gap-2 ">
+                {post.comments.map((comment) => (
+                  <div
+                    key={comment.id}
+                    className="flex flex-col gap-4 border hover:shadow-md p-2 rounded-lg"
+                  >
+                    <div className="flex gap-2 items-center">
+                      <User2 className="size-8 border rounded-full p-1" />
+                      <h4 className="font-semibold text-gray-800 text-sm ">{comment.email}</h4>
+                    </div>
+                    <p className="text-sm">{comment.body}</p>
+                  </div>
+                ))}
+              </div>
+            </ScrollArea>
+          </DialogContent>
+        </Dialog>
       </div>
     </div>
   );
